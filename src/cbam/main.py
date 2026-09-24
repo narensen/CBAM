a8452581cc958dc5be45b2edd293aa78dcbb3451
Naren Sengodan <scartegy@gmail.com>
2026-09-24 17:27:07 +0530

    INFO : CBAM Complete

--- src/cbam/main.py
+++ src/cbam/main.py
@@ -1,1 +1,14 @@
-print("Hello from cbam!")
+from cbam.attentions import *
+
+class CBAM(nn.Module):
+
+    def __init__(self, channels : int, reduction : int = 16, kernel_size: int = 7):
+        super().__init__()
+        self.channel = ChannelAttention(channels, reduction)
+        self.spatial = SpatialAttention(kernel_size)
+
+    def forward(self, x):
+        x = x * self.channel(x)
+        x = x * self.spatial(x)
+        return x
+    
